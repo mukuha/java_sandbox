2af3e9d357d6fda3a195b64b91a74ac015438786
Antony Mukuha <someone@nowhere.com>
2022-06-03 18:33:36 +0300

    Adds a documentation for the project

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mukuha.javasandbox","l":"Data"},{"p":"com.mukuha.javasandbox","l":"Main"},{"p":"com.mukuha.javasandbox","l":"MathHelper"}];updateSearchResults();
