43e5810fbd00dcfe23ac871e73c4af0d3ef367e3
Antony Mukuha <someone@nowhere.com>
2022-06-03 09:18:11 +0300

    Moves the classes Main.java and Utilities.java from the com.example.java package to src directory and deletes that package

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package com.example.java;
-
 import java.math.BigDecimal;
 
 public class Main {
--- src/Utilities.java
+++ src/Utilities.java
@@ -1,5 +1,3 @@
-package com.example.java;
-
 import java.math.BigDecimal;
 import java.util.Scanner;
 
